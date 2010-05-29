3a5d24799b06d41c28299ccde387445f22736fb5
Dylan Clendenin <dylan.clendenin@gmail.com>
2010-05-29 16:04:39 -0700

    attempting mongohq setup

--- setup.rb
+++ setup.rb
@@ -1,27 +1,8 @@
-#begin
-#  require "mongo"; include Mongo
-#rescue LoadError
-#  puts "You need the mongo gem\nTry: gem install mongo\n"
-#  exit(1)
-#end
-#
-#begin
-#  db = Connection.new("localhost", Connection::DEFAULT_PORT).db("openanno")
-#  Anno = db.collection("anno")
-#  AnnoAdd = db.collection("annoadd")  # { "anno_id" : "anno_user" } where anno_user is the last to modify
-#  User = db.collection("user")
-#rescue Mongo::ConnectionFailure
-#  puts "Make sure MongoDB daemon (mongod) is running\nTry: mongod\n"
-#  exit(1)
-#end
+ENV["MONGOHQ_URL"] ||= "localhost"
 
+require "uri"
+require "mongo"
 
-# some harmless extensions to the mongo ruby client lib that i like
-#def Anno.all
-#  Vocabulary.find.to_a
-#end
-#
-#def Anno.empty?
-#  (Vocabulary.count == 0 ? true : false)
-#end
-# 
+uri = URI.parse(ENV["MONGOHQ_URL"])
+conn = Mongo::Connection.new(ENV["MONGOHQ_URL"], Mongo::Connection::DEFAULT_PORT)
+db = conn.db(uri.path.gsub(/^\//, ""))
