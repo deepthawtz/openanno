2404a13ebfc0dcc1f102ee4f928d4f7ffc981371
Carlos Cardona <carlos@zazengo.com>
2010-05-30 00:58:00 -0700

    updated css to match zak's design

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,4 @@
 // our app js code here, go for it.
 $(document).ready(function(){
-  $("li:odd").css("background-color", "#ddd")
-  $("li:even").css("background-color", "#eee")  
+  $("#accordion").accordion()
 })
